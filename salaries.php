8b9223ac5cd5a6da55aee40fc39580e8c75c9609
Peter Anokwu <peterfrancisng@yahoo.com>
2016-10-20 17:01:05 +0100

    2nd commit

---
+++ salaries.php
@@ -0,0 +1,55 @@
+
+
+
+<?php 
+  include('includes/header.php');
+?>
+
+<body>
+<?php 
+  include('includes/nav.php');
+?>
+<br />
+<br />
+	
+		
+		<br />
+    
+	        
+	         
+	
+			<div class="row">
+			<div class="col-xs-12 col-lg-1">
+			</div>
+			<div class="col-xs-12 col-lg-11">
+					<h2 class="pull-left" style="color:#476c0d; font-weight:bold;">My Friends Profiles</h2>
+					
+						
+						
+						
+						
+						
+						
+						
+						
+						
+						
+			</div>
+	</div>
+	
+
+	
+	
+	
+	
+	
+	
+	</div>
+	
+	 
+		<br/><br/>
+	
+					
+<?php 
+  include('includes/footer.php');
+?>
