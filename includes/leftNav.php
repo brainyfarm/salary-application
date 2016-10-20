a9ecc32b5ced4816b3663ebd59266b034a9c16c6
faksam <fakunlesamuel@gmail.com>
2016-10-20 23:54:13 +0100

    Final

---
+++ includes/leftNav.php
@@ -0,0 +1,53 @@
+<nav id="sideNavParent" class="pdsa-sn-wrapper">
+				<ul>
+					<li class="pdsa-sn-brand">
+						<a href="../index.php" class="visible-sm visible-md visible-lg">Home (Portal)</a> 
+					</li>
+					<li>
+						<a href="#" 
+							data-toggle="collapse"
+							data-target="#ullogins">
+							<span class="visible-sm visible-md visible-lg">Login Here&nbsp;<b class="caret"></b></span>
+							<a href="mobilelogin.php"><i class="glyphicon glyphicon-list-alt visible-xs"></i></a> 
+						</a>
+						<div class="visible-sm visible-md visible-lg">
+							<ul id="ullogins" class="collapse" data-parent="#sideNavParent">
+								<li>
+									<a href="student/studentlogin.php">
+										Student Login
+									</a>
+								</li>
+								<li>
+									<a href="staff/stafflogin.php">
+										Staff Login
+									</a>
+								</li>
+							</ul>
+						</div>
+					</li>
+					<li>
+						<a href="#" 
+							data-toggle="collapse"
+							data-target="#ulregisters">
+							<span class="visible-sm visible-md visible-lg">Register Here&nbsp;<b class="caret"></b></span>
+							<a href="mobileregister.php"><i class="glyphicon glyphicon-globe visible-xs"></i></a>
+						</a>
+						<div class="visible-sm visible-md visible-lg">
+							<ul id="ulregisters" class="collapse" data-parent="#sideNavParent">
+								<li>
+									<a href="student/studentregister.php">
+										Student Account
+									</a>
+								</li>
+								<li>
+									<a href="staff/staffregister.php">
+										Staff Account
+									</a>
+								</li>
+							</ul>
+						</div>
+					</li>
+					
+				</ul>
+				
+			</nav>
---
+++ salary-data.js
@@ -0,0 +1,11 @@
+var salary = {
+	/**
+     * Employee salary by category
+     * @type Number
+     */
+    "interns": 50000,
+   	"associates": 65000,
+   	"managers": 100000,
+   	"executives": 115000,
+   	"directors": 130000
+};
