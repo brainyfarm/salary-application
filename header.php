d2005ae9e08acbbd2f0c6a49172a075c792f1774
Peter Anokwu <peterfrancisng@yahoo.com>
2016-10-20 16:42:53 +0100

    front end files

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Friend Network System</title>
+  <link href="css/bootstrap.min.css" rel="stylesheet" />
+  <link href="css/Site.css" rel="stylesheet" />
+  <link href="css/pdsa-sidebar.css" rel="stylesheet" />
+  <link rel="stylesheet" href="css/yamm.css">
+  <link rel="stylesheet" href="css/bootstrap-social.css">
+  <link rel="stylesheet" id="font-awesome-css" href="css/font-awesome.min.css" type="text/css" media="screen">
+  <script src="js/script.js"></script>
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="js/html5shiv.min.js"></script>
+      <script src="js/respond.min.js"></script>
+    <![endif]-->
+</head>
---
+++ includes/resultLeftNav.php
@@ -0,0 +1,89 @@
+<nav id="sideNavParent" class="pdsa-sn-wrapper">
+				<ul>
+					<li class="pdsa-sn-brand">
+						<a href="../index.php" class="visible-sm visible-md visible-lg">Home (Portal)</a> 
+					</li>
+					<li>
+						<a href="#" 
+							data-toggle="collapse"
+							data-target="#ullogins">
+							<span class="visible-sm visible-md visible-lg">Subjects&nbsp;<b class="caret"></b></span>
+							<a href="mobilelogin.php"><i class="glyphicon glyphicon-list-alt visible-xs"></i></a> 
+						</a>
+						<div class="visible-sm visible-md visible-lg">
+							<ul id="ullogins" class="collapse" data-parent="#sideNavParent">
+								<li>
+									<a href="uploadresult1.php">
+										English Language
+									</a>
+								</li>
+								<li>
+									<a href="staff/stafflogin.php">
+										Mathematics
+									</a>
+								</li>
+								<li>
+									<a href="parent/parentlogin.php">
+										Civic Education
+									</a>
+								</li>
+							</ul>
+						</div>
+					</li>
+					<li>
+						<a href="#" 
+							data-toggle="collapse"
+							data-target="#ulregisters">
+							<span class="visible-sm visible-md visible-lg">Register Here&nbsp;<b class="caret"></b></span>
+							<a href="mobileregister.php"><i class="glyphicon glyphicon-globe visible-xs"></i></a>
+						</a>
+						<div class="visible-sm visible-md visible-lg">
+							<ul id="ulregisters" class="collapse" data-parent="#sideNavParent">
+								<li>
+									<a href="student/studentregister.php">
+										Student Account
+									</a>
+								</li>
+								<li>
+									<a href="staff/staffregister.php">
+										Staff Account
+									</a>
+								</li>
+								<li>
+									<a href="parent/parentregister.php">
+										Parent Account
+									</a>
+								</li>
+							</ul>
+						</div>
+					</li>
+					<li>
+						<a href="#" 
+							data-toggle="collapse"
+							data-target="#ulemails">
+							<span class="visible-sm visible-md visible-lg">Email Login&nbsp;<b class="caret"></b></span>
+							<a href="email_login.php"><i class="glyphicon glyphicon-list-alt visible-xs"></i></a> 
+						</a>
+						<div class="visible-sm visible-md visible-lg">
+							<ul id="ulemails" class="collapse" data-parent="#sideNavParent">
+								<li>
+									<a href="#">
+										WebMail Login
+									</a>
+								</li>
+								<li>
+									<a href="#">
+										YahooMail Login
+									</a>
+								</li>
+								<li>
+									<a href="#">
+										Gmail Login
+									</a>
+								</li>
+							</ul>
+						</div>
+					</li>
+				</ul>
+				
+			</nav>
