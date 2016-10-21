9249dbd2d46bb0de41e85af8cdca0534320e3a22
Olawale Akinseye <brainyfarm@gmail.com>
2016-10-21 19:27:19 +0100

    Revert "Update calculateSalary.js"
    This reverts commit ab35db0ba7b964ce9a6de788545eb351d069289a.

--- calculateSalary.js
+++ calculateSalary.js
@@ -1,12 +1,7 @@
-var btn = document.getElementById("calc");
-  btn.onclick = function() {
-    var salary = '{"intern": 50000,"associate": 65000,"manager": 100000,"executive": 11500,"director": 130000}';
-    var noOfyears = document.getElementById("yearofexp").value;
-    var e = document.getElementById("employeelevel");
-var employeeLevel = e.options[e.selectedIndex].value;
-var text = e.options[e.selectedIndex].text;
+function calculateSalary(employeeLevel,noOfyears){
   var json = JSON.parse(salary);
-  var yearlySalary = json[String(employeeLevel)];
-  var totalSalary = yearlySalary * noOfyears;
+  var yearlySalary = json[employeeLevel];
+  var totalSalary = yearlySalary * noOfyrs;
 
-document.getElementById("salaryReport").innerHTML = "The total salary for "+employeeLevel+" with "+noOfyears+" is: "+totalSalary;
+  return totalSalary;
+}
