ab35db0ba7b964ce9a6de788545eb351d069289a
faksam <fakunlesamuel@gmail.com>
2016-10-21 16:30:46 +0100

    Update calculateSalary.js

--- calculateSalary.js
+++ calculateSalary.js
@@ -1,7 +1,12 @@
-function calculateSalary(employeeLevel,noOfyears){
+var btn = document.getElementById("calc");
+  btn.onclick = function() {
+    var salary = '{"intern": 50000,"associate": 65000,"manager": 100000,"executive": 11500,"director": 130000}';
+    var noOfyears = document.getElementById("yearofexp").value;
+    var e = document.getElementById("employeelevel");
+var employeeLevel = e.options[e.selectedIndex].value;
+var text = e.options[e.selectedIndex].text;
   var json = JSON.parse(salary);
-  var yearlySalary = json[employeeLevel];
-  var totalSalary = yearlySalary * noOfyrs;
+  var yearlySalary = json[String(employeeLevel)];
+  var totalSalary = yearlySalary * noOfyears;
 
-  return totalSalary;
-}
+document.getElementById("salaryReport").innerHTML = "The total salary for "+employeeLevel+" with "+noOfyears+" is: "+totalSalary;
